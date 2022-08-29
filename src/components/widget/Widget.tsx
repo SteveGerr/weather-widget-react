ba0ea9fd8749e05053e324e5a5888d0632bdd618
SteveGerr <prikolsamara@mail.ru>
2022-08-29 21:59:34 +0400

    [refactor] Add img import

--- src/components/widget/Widget.tsx
+++ src/components/widget/Widget.tsx
@@ -1,6 +1,10 @@
 // import { useState, useEffect } from 'react';
 import "./Widget.scss"
 
+import humidity from "../../assets/drop-silhouette.png"
+import windSpeed from "../../assets/wind-solid.svg"
+import gear from "../../assets/gear-solid.svg"
+
 const Widget = () => {
 
 
@@ -9,7 +13,7 @@ const Widget = () => {
           <div className="widget__menu">
             <div className="widget__city"><span className="widget__city-name">city.cty</span>, city.country</div>
             <button className="widget__settings-btn">
-              <img className="widget__settings-img" src="../assets/gear-solid.svg" alt="gear" />
+              <img className="widget__settings-img" src={gear} alt="gear" />
             </button>
           </div>
 
@@ -26,11 +30,11 @@ const Widget = () => {
           <div className="widget__weather-params">
             <div className="widget__weather-params-item">feels like: city.feels_like°</div>
             <div className="widget__weather-params-item">
-              <img className="widget__weather-img" src="../assets/drop-silhouette.png" alt="humidity" />
+              <img className="widget__weather-img" src={humidity} alt="humidity" />
               <span> city.humidity %</span>
             </div>
             <div className="widget__weather-params-item">
-              <img className="widget__weather-img" src="../assets/wind-solid.svg" alt="wind speed" />
+              <img className="widget__weather-img" src={windSpeed} alt="wind speed" />
               <span>city.wind  m/s</span>
             </div>
           </div>

a8e084e2cbced07b572bc99726074c71f9438a49
SteveGerr <prikolsamara@mail.ru>
2022-08-31 18:37:17 +0400

    [feat] Add data type

---
+++ src/interfaces/WeatherDataT.ts
@@ -0,0 +1,13 @@
+export type weatherData = {
+    id: number,
+    /** city */
+    city: string,
+    country: string,
+    feels_like: number,
+    humidity: number,
+    pressure: number,
+    temp: number,
+    wind: number,
+    icon: string,
+    desc: string
+  }
